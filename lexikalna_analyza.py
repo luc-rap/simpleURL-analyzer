f7770a6efa96186d61f31d82347768a606f6c4ef
Lucia Rapánová <lucia.rapanova@gmail.com>
2024-05-01 15:28:58 +0200

    fixed some states

--- lexikalna_analyza.py
+++ lexikalna_analyza.py
@@ -20,15 +20,20 @@ dfa = DFA(
     input_symbols={'a', 'b', 'h', 't', 'p', 'e'},
     transitions={
         'q0': {'a': 'q0', 'b': 'q0', 'h': 'qH', 't':'q0', 'p':'q0', 'e':'q0'},
-        'qH': {'a': 'q0', 'b': 'q0', 'h': 'q0', 't':'qT', 'p':'q0', 'e':'q0'},
-        'qT': {'a': 'q0', 'b': 'q0', 'h': 'q0', 't':'qT1', 'p':'q0', 'e':'q0'},
-        'qT1': {'a': 'q0', 'b': 'q0', 'h': 'q0', 't':'q0', 'p':'qP', 'e':'q0'},
-        'qP': {'a': 'q0', 'b': 'q0', 'h': 'q0', 't':'q0', 'p':'q0', 'e':'q0'}
+        'qH': {'a': 'q0', 'b': 'q0', 'h': 'qH', 't':'qT', 'p':'q0', 'e':'q0'},
+        'qT': {'a': 'q0', 'b': 'q0', 'h': 'qH', 't':'qT1', 'p':'q0', 'e':'q0'},
+        'qT1': {'a': 'q0', 'b': 'q0', 'h': 'qH', 't':'q0', 'p':'qP', 'e':'q0'},
+        'qP': {'a': 'q0', 'b': 'q0', 'h': 'qH', 't':'q0', 'p':'q0', 'e':'q0'}
     },
     initial_state='q0',
-    final_states={'qP'}
+    final_states={'q0','qP'}
 )
-
+# ahttpb = [a, http, b]
+# htttp = [h, t, t, t, p]
+# http = [http]
+# hhttp = [h, http]
+# haha = [h, a, h, a]
+# httpp = [http, p]
 try:
     while True:
         tokens = []
@@ -44,10 +49,20 @@ try:
             if next_state is not None:
                 current_state = next_state
                 if current_state == 'qH':
+                    # Ked sme v stave qH tak vycistime vsetko, co sme mali predtym
+                    # Napriklad case ked pride hhttp, aby sme mali = [h, http]
+                    if current_token:
+                        for i in current_token:
+                            tokens.append(i)
+                        current_token = ''
+                    # Ked sme vycistili co to bolo, zacneme citat "http"
                     for i in current_token:
                         tokens.append(i)
                         current_token = ''
+
                 if current_state == 'qP':
+                    # sme v stave qP - precitali sme http 
+                    # Ulozime token a vycistime 
                     current_token += symbol
                     print("We found http token")
                     tokens.append(current_token)
@@ -55,13 +70,21 @@ try:
                     print("---------")
                     current_token = ''
                 else:
+                    # Ak sme niekde inde, tak len pridame symbol do tokenu
                     current_token += symbol
                     print("Appending token...")
                     print(current_token)
         if current_token:
+            # Ak sme skoncili citat a mame nejaky token, tak ho pridame do zoznamu
             for i in current_token:
                 tokens.append(i)
         print(tokens)
+        # Ak sme skoncili a mame finalny stav, akceptujeme vstup 
+        # Toto este trochu nefunguje dobre lebo som sa stratila :D 
+        if current_state in dfa.final_states:
+            print("Accepted")
+        else:
+            print("Rejected")
 
         #if dfa.accepts_input(input('Please enter your input: ')):
             # 
