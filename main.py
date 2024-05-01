2f385ab6bd122a1d86922e9d3649d51133ac58f6
Lucia Rapánová <lucia.rapanova@gmail.com>
2024-05-01 22:44:08 +0200

    updated main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# precita vstup od usera alebo zo suboru alebo whatever, potom zavola tokanizer a potom syntakticku analyzu
+from syntakticka_analyza import *
+from lexikalna_analyza import *
+
+def main():
+    vstup = input("Zadajte vstup: ")
+# ... TO DO ...
