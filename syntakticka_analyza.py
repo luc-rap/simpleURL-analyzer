5ccab57ea2fcf908e05a30e28af9db94c0ef258d
Lucia Rapánová <lucia.rapanova@gmail.com>
2024-05-05 14:02:36 +0200

    added comments

--- syntakticka_analyza.py
+++ syntakticka_analyza.py
@@ -10,7 +10,7 @@ stack.append('Z0') # spodok zasobnika
 stack.append(start) # startovaci symbol
 
 # input string: http://google.com
-input_tokens = ['http://', 'g', 'o', 'o', 'g', 'l', 'e', '.', 'c', 'o', 'm']
+input_tokens = ['http://', 'google', '.', 'com']
 
 def parse(input_tokens):
     print("Current stack: " + str(stack))
@@ -18,7 +18,7 @@ def parse(input_tokens):
     top = stack.pop()
     print("Current top: " + top)
 
-    # ktory token prave citame zo vstupu:
+    # ktory token prave citame zo vstupu: ['http://', 'google', '.', 'com'] <- 4 tokeny
     current_token = input_tokens.pop(0)
     print(current_token)
 
@@ -28,25 +28,27 @@ def parse(input_tokens):
         # get teminal symbol from the token use terminals_symbols dictionary 
         if current_token in terminals_symbols or current_token in terminals_alpha or current_token in terminals_digits:
             if current_token in terminals_symbols:
-                symbol = terminals_symbols[current_token]
+                symbol = terminals_symbols[current_token] # musime ho prelozit na token ktory mame v pravidlach http:// -> http 
             else:
                 symbol = current_token
-            print(symbol)
+            print(symbol) # symbol je teda current token
+        else:
+            pass # TBD ?? 
 
         #  elif current_token in non_terminals:
             #symbol = current_token
             #print(symbol)
 
-        if top == symbol:
+        if top == symbol: # ak na vstupe je to iste ako na vrchu zasobnika, tak popujeme a posuvame sa dalej v citani vstupu 
             print("Match")
             # pop top from the stack
             top = stack.pop()
             print("Current top: " + top)
             print("Current stack: " + str(stack))
-            # read the next token from the input string
+
             # posuvame sa len ked na vrchu zasobnika je taky isty symbol ako prave citame zo vstupu 
-            if input_tokens:
-                current_token = input_tokens.pop(0)
+            if input_tokens: # ak je nieco na vstupe 
+                current_token = input_tokens.pop(0) # citaj dalsi token zo vstupu 
                 print(current_token)
                 symbol = current_token
             else:
@@ -54,7 +56,7 @@ def parse(input_tokens):
             # connect top + current token with the parsing table to get the rule number 
         print(top, symbol)
 
-        if top == 'eps':
+        if top == 'eps': # ked na vrchu iba eps tak popujeme a ideme dalej
             print("Skipping...")
             # pop top from the stack
             top = stack.pop()
@@ -71,24 +73,24 @@ def parse(input_tokens):
                 else:
                     print("Something went terribly wrong?")
 
-        if current_token in terminals_symbols or current_token in terminals_alpha or current_token in terminals_digits:
+        if current_token in terminals_symbols or current_token in terminals_alpha or current_token in terminals_digits: # viem ze toto je znovu ale este sa zamyslim nad tym 
             if current_token in terminals_symbols:
                 symbol = terminals_symbols[current_token]
             else:
                 symbol = current_token
             print(symbol)
         if symbol in terminals_alpha:
-            token = "letter"
+            token = "letter" # si to "prekodujeme" aby sme nemuseli kazde jedno cislo alebo pismenko zapisovat
         elif symbol in terminals_digits:
             token = "number"
         else:
             token = symbol
         print(top, token)
-        rule_number = parsing_table[top][token]
+        rule_number = parsing_table[top][token] # az tu actually citame pravidlo z tabulky 
         print(rule_number)
         # get the rule from the rules dictionary
-        rule = rules[rule_number-1]
-        if rule == terminals_alpha or rule == terminals_digits:
+        rule = rules[rule_number-1] # kvoli tomu ze tabulka je cislovana od nuly a pravidla od jednotky
+        if rule == terminals_alpha or rule == terminals_digits: # ak je to len pravidlo 42 a 43 tak to appendujeme do zasobnika
             print("ahaha")
             stack.append(symbol)
             print("Current stack: " + str(stack))
@@ -99,6 +101,7 @@ def parse(input_tokens):
 
             print(rule[0])
             # push rule to stack from last to first
+            # ak je to ine pravidlo tak zapisujeme ho do zasobnika od konca napr ["http hostport httpaddr_1"] -> ["httpaddr_1", "hostport", "http"] 
             for rule in rule[::-1]:
                 stack.append(rule)
             print("Current stack: " + str(stack))
