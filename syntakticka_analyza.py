f8c41995afc5ffb648c0431eb6a39fce00f935ca
tokhis <tokhi.sofia@gmail.com>
2024-05-05 22:12:56 +0200

    Syntax analysis prototype (also the tokens are now specific letters and we have a gitgnore)
    Co-authored-by: Lucia Rapánová <luciarap@users.noreply.github.com>

--- syntakticka_analyza.py
+++ syntakticka_analyza.py
@@ -1,5 +1,4 @@
 from collections import deque
-
 from pravidla import * 
 
 stack = deque()
@@ -9,119 +8,95 @@ start = 'url'
 stack.append('Z0') # spodok zasobnika
 stack.append(start) # startovaci symbol
 
-# input string: http://google.com
-input_tokens = ['http://', 'g', 'o', 'o', 'g', 'l', 'e', '.', 'c', 'o', 'm']
+input_tokens = ['http://', 'g', 'o', 'o', 'g', 'l', 'e', '.', 'c', 'o', 'm', '/']
+
+# todo: osetrit nespravny vstup
+
+# get teminal symbol from the token use terminals_symbols dictionary -- either a terminal or "name"
+def get_terminal(current_token):
+    if current_token in terminals_symbols:
+        return terminals_symbols[current_token]
+
+    # our tokens are protocols + symbols + "name"
+    # protocols + symbols are in terminals_symbols (and all tokens here are valid) => the other symbol must be "name"
+    elif current_token in terminals_digits:
+        return 'number'
+    
+    elif current_token in terminals_alpha:
+        return 'letter'
+
+    elif current_token == '$':
+        return '$'
+
+    else:
+        return 'something has gone horribly wrong o_o'
+    
 
 def parse(input_tokens):
-    print("Current stack: " + str(stack))
+    input_tokens.append('$')
+    current_token = input_tokens.pop(0)  # ktory token prave citame zo vstupu:
+    print(f'INITIAL INFO\nstack - {str(stack)} / token - {current_token}')
 
-    top = stack.pop()
-    print("Current top: " + top)
-
-    # ktory token prave citame zo vstupu:
-    current_token = input_tokens.pop(0)
-    print(current_token)
+    #! current_token = ktory token prave citame zo vstupu
+    #! token = aky token je to naozaj (terminals vs number vs letter)
 
     while stack:
-        print("Current token at the beginning of while loop")
-        print(current_token)
-        # get teminal symbol from the token use terminals_symbols dictionary 
-        if current_token in terminals_symbols or current_token in terminals_alpha or current_token in terminals_digits:
-            if current_token in terminals_symbols:
-                symbol = terminals_symbols[current_token]
-            else:
-                symbol = current_token
-            print(symbol)
+        top = stack.pop()
+        token = get_terminal(current_token)
+        print(f'------- while begin ------- \nstack - {str(stack)} / top - {top} / current_token - {current_token} / token - {token}')
 
-        #  elif current_token in non_terminals:
-            #symbol = current_token
-            #print(symbol)
+        # check until symbol on top != current token (remove the symbol on top in the meanwhile)
+        # posuvame sa len ked na vrchu zasobnika je taky isty symbol (token) ako prave citame zo vstupu 
+        while (top == token):
+            print(f'in token == top{top, token}')
+            top = stack[-1]  
+            stack.pop()  # only after we know the actual top character
+            print(f'Match (top == token)\n Top: {top} --- Stack: {str(stack)} --- Token: {token}')
 
-        if top == symbol:
-            print("Match")
-            # pop top from the stack
-            top = stack.pop()
-            print("Current top: " + top)
-            print("Current stack: " + str(stack))
             # read the next token from the input string
-            # posuvame sa len ked na vrchu zasobnika je taky isty symbol ako prave citame zo vstupu 
             if input_tokens:
                 current_token = input_tokens.pop(0)
-                print(current_token)
-                symbol = current_token
+                token = get_terminal(current_token)
+                print(f'Current token (if input tokens exist): {token}')
             else:
                 break
-            # connect top + current token with the parsing table to get the rule number 
-        print(top, symbol)
 
+        # eps check - if eps found, skip (pop top from the stack)
         if top == 'eps':
-            print("Skipping...")
-            # pop top from the stack
             top = stack.pop()
-            print("Current top: " + top)
-            print("Current stack: " + str(stack))
+            print(f'Skipping...\n Top: {top} --- Stack: {str(stack)} --- Token: {token}')
+        
+        # we've reached the bottom of the stack and ran out of input tokens :)
+        if top == 'Z0' and not input_tokens:
+            print('Accepted :)')
+            break
 
-            if top == 'Z0':
-          # ~ TBD lebo toto je este zle :D dolar tam nema ostat uz je vela hodin sleepy tired brain
-                print(input_tokens)
-                # if we popped and z0 is on top and input_tokens is dollar, we are done
-                if input_tokens == ['$']:
-                    print("Accepted")
-                    break
-                else:
-                    print("Something went terribly wrong?")
-
-        if current_token in terminals_symbols or current_token in terminals_alpha or current_token in terminals_digits:
-            if current_token in terminals_symbols:
-                symbol = terminals_symbols[current_token]
-            else:
-                symbol = current_token
-            print(symbol)
-        if symbol in terminals_alpha:
-            token = "letter"
-        elif symbol in terminals_digits:
-            token = "number"
-        else:
-            token = symbol
-        print(top, token)
-        rule_number = parsing_table[top][token]
-        print(rule_number)
         # get the rule from the rules dictionary
-        rule = rules[rule_number-1]
+        rule_number = parsing_table[top][token]
+        rule = rules[rule_number - 1]
+        print(f'RULES\nTop - {top} / Token - {token} / Rule number - {rule_number} / Rule - {rule}')
+
+        # add a specific alphanumeric character instead of the first one of terminals_alpha or terminals_digits
         if rule == terminals_alpha or rule == terminals_digits:
-            print("ahaha")
-            stack.append(symbol)
-            print("Current stack: " + str(stack))
+            stack.append(token)
+
+        # don't add an epsilon to stack if it's an epsilon rule
+        elif rule == 'eps':
+            continue
+
+        # split the rule into mulitple strings if it's not just one
         else:
-        # rule 42 su vsetky pismenka, nechceme tento rule appendovat do zasobniku, chceme appendnut iba symbol (ak je token letter)
-            rule = rule[0].split()
-            print(rule)
+            rule = rule.split(' ')
+            rule.reverse()  # reverse the rules because it's a stack
+            stack.extend(rule)
 
-            print(rule[0])
-            # push rule to stack from last to first
-            for rule in rule[::-1]:
-                stack.append(rule)
-            print("Current stack: " + str(stack))
-        top = stack.pop()
-        print(top)
+        print(f'Current stack: {str(stack)}')
 
-    # if we reach the end of the input string and the stack is empty, we are done
-
-    # ked to docita potom idu este dolare a malo by to byt tak ze sa to uz len dopopuje
-    # $ hostname - > rule 22 -> eps -> pop 
-
-        if not input_tokens:
-            input_tokens.append('$')
-    
-    # if we reached end of input but stack is not empty - reject the input
+        
     if not input_tokens and stack:
         print("Rejected")
+
+    print(f'we\'re at the end of the function yay yippee')
     
-    # if we reach bottom of the stack (Z0) and input is empty, we are done
-
-        # push the rule to the stack
-        #for symbol in reversed(rules[rule]):
-        #    stack.append(symbol)
-
 
 parse(input_tokens)
