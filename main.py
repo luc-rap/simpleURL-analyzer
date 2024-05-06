e51565e41632bc1f97b8300af2d390c2424250c9
Lucia Rapánová <lucia.rapanova@gmail.com>
2024-05-06 13:18:31 +0200

    added keyboard interrupt

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from lexikalna_analyza import *
 
 def main():
     while True:
-        print("Lexikalny a syntakticky analyzator\n")
-        print("Zadajte vstup alebo stlacte ctrl+c pre ukoncenie programu\n")
-        vstup = input("Zadajte vstup: ")
-        print("TOKENIZATOR\n")
-        tokens = tokenize(vstup, dfa)
-        if not tokens:
-            print("Vstup bol rejected\n")
-            continue
-        print(f"Tokenizovany vstup: {tokens}")
-        print("SYNTAKTICKA ANALYZA\n")
-        parse(tokens)
+        try:
+            print("Lexikalny a syntakticky analyzator\n")
+            print("Zadajte vstup alebo stlacte ctrl+c pre ukoncenie programu\n")
+            vstup = input("Zadajte vstup: ")
+            print("TOKENIZATOR\n")
+            tokens = tokenize(vstup, dfa)
+            if not tokens:
+                print("Vstup bol rejected\n")
+                continue
+            print(f"Tokenizovany vstup: {tokens}")
+            print("SYNTAKTICKA ANALYZA\n")
+            parse(tokens)
+        except KeyboardInterrupt:
+            print("\nProgram bol ukonceny")
+            break
+
 
 if __name__ == "__main__":
     main()
