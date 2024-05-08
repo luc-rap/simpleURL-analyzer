eec315b7f31343d94b96085d28a2079d5d709268
tokhis <xshatokhina@stuba.sk>
2024-05-08 12:40:46 +0200

    Created a working zotavenie 4 and checked it against the incorrect inputs from lexikalna_analyza.py (ft. @luciarap)

--- lexikalna_analyza.py
+++ lexikalna_analyza.py
@@ -15,7 +15,8 @@ examples_correct = {
     9: 'http://youtu.be/dQw4w9WgXcQ',
     10: 'http://hostname:12345',
     11: 'telnet://lucien:password@vanhohenheim:13111988',
-    13: 'http://softverovejazyky:2324/su/najlepsi/predmet/na/svete/frfr'
+    13: 'http://softverovejazyky:2324/su/najlepsi/predmet/na/svete/frfr',
+    14: 'ftp://lucien@vanhohenheim.fr/islands/'
 }
 
 examples_wrong_lexical = {
@@ -27,21 +28,22 @@ examples_wrong_lexical = {
 }
 
 examples_wrong_syntax = {
-    0: 'http://google.',
-    1: 'mailto::lucien',
-    2: 'telnet://vanhohenheim',
-    # todo: idea: + instead of / in 3
-    3: 'http://website.com?search/wrongorder',  # 3 -- ?search/wrongorder -> ?searchwrongorder
-    '3.1': 'http://w.c?s/a+b',  # zotavenie 3
-    4: 'ftp://lucien:@hostport',
-    5: 'ftp://lucien:password',
-    6: 'http://hostname:port',
-    7: 'ftp://:lucien@gmail.com',
-    8: 'telnet://lucien:password@vanhoheinheim:1311.1988',
-    9: 'ftp://cantsearchthis.com/donttrythisathome?howtopassschoolandnotpassaway',
-    10: 'ftp://lucien@vanhohenheim.fr/islands/',
-    11: 'ftp://lucien:password@hostport'
+    0: 'http://google.', # rejectne lebo za bodkou musi byt aspon jeden alfanumericky znak
+    1: 'mailto::lucien', # zotavenie 4 doplni @, ale stale rejected, lebo chyba alfanumer. znak. bez zotavenia chyba @ tak rejectne
+    2: 'telnet://vanhohenheim', # lebo chyba login a zavinac hostport, nic to nebude recoverovat 
+    3: 'http://website.com?search/wrongorder',  # 3 - accepted (skpine /wrongorder lebo caka + alebo eps), 4 - rejected, nic sa nedoplni, nevie rozpoznat /
+    '3.1': 'http://w.c?s/a+b',  # zotavenie 3 preskoci slash a natrafi na + ktore vie akceptovat (cize pokracuje v citani od +), 4 rejectne, lebo nepozna /
+    4: 'ftp://lucien:@hostport',  # zotavenie 3 preskoci @ (kedze ocakava password po :), docita to hostport ale rejectne, lebo chyba @hostport/
+    '4.5': 'ftp://lucien:@hostport@nieco/', # zotavenie 3 preskorci @ a akceptuje vstup
+    5: 'ftp://lucien:password',  # 4 doplni @, ale stale to rejectne
+    6: 'http://hostname:port',  # nemozeme mat alphabeticky port
+    7: 'ftp://:lucien@gmail.com', # ftp preskoci : ale chyba slash tak to stale bude zle  (3) (3 - preskoci :, chyba slash, 4 - : navyse)
+    8: 'telnet://lucien:password@vanhoheinheim:1311.1988',  # :3
+    9: 'ftp://cantsearchthis.com/donttrythisathome?howtopassschoolandnotpassaway', # ftp ocakava :password alebo @ bez passwordu 
+    10: 'ftp://lucien@vanhohenheim.fr', # 4 - chyba slash - mozeme pridat v recovery slash 
+    11: 'ftp://lucien:password@hostport' # 4 - chyba slash 
 }
+# ftp add slash
 
 
 dfa = DFA(
